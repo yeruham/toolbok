c06b7cff690a1c8cf3a18260a3c2a9507cf45896
YeruhamMendelson <y0583207549@gmail.com>
2025-05-19 15:30:56 +0300

    basic tool

---
+++ tool.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Tool
+{
+    private string _name;
+    private string _weight;
+
+    public void Describe()
+    {
+        Console.WriteLine("this is tool");
+    }
+
+    public void Use()
+    {
+        Console.WriteLine("Tool is being used.");
+    }
+}
